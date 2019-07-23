0ca54fc2dcddeb171ad25cc630d54ac0ef8db6d8
Marlon Rebello Viana <marlonrviana@gmail.com>
2019-07-22 21:49:14 -0300

    COMPLETO
    Já posso enviar, agora as melhorias podem ser priorizadas

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
- $(function() {
- 	$('.scroll-down').click (function() {
- 		$('html, body').animate({scrollTop: $('#PrimeiroTexto').offset().top-100 }, 'slow');
- 		return false;
- 	});
- });
+$(function() {
+	$('.scroll-down').click (function() {
+		$('html, body').animate({scrollTop: $('#PrimeiroTexto').offset().top-100 }, 'slow');
+		return false;
+	});
+});
